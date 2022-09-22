21f90a0c257f98b72ae24d46d8cad92cf88100f5
Will Olson <will.olson11@gmail.com>
2022-09-22 13:09:54 -0700

    Add mappings to fuzzy search common directories

--- lua/keys.lua
+++ lua/keys.lua
@@ -1,12 +1,19 @@
 --[[ keys.lua ]]
-local map = vim.api.nvim_set_keymap
+local map = require('utils').map
 
 -- remap the key used to leave insert mode
-map('i', 'jk', '<Esc>', {})
-map('t', 'jk', '<C-\\><C-n>', {})
+map('i', 'jk', '<Esc>', {})                                 -- Escape insert mode
+map('t', 'jk', '<C-\\><C-n>', {})                           -- Escape terminal mode
 
 -- Toggles
 map('n', '<leader>n', [[:NvimTreeToggle]], {})
 map('n', '<leader>l', [[:IndentLinesToggle]], {})
 map('n', '<leader>t', [[:TagbarToggle]], {})
+
+-- Misc
+map('i', '<leader><cr>', '<Esc>yyp<C-a>elC<Space>', {})     -- Auto increment markdown ordered list
+
+-- Fuzzy Find
 map('n', '<leader>ff', [[:Telescope find_files]], {})
+map('n', '<leader>hf', [[:lua require('telescope.builtin').find_files({cwd='~/'})<cr>]], {})
+map('n', '<leader>vf', [[:lua require('telescope.builtin').find_files({cwd='~/.config/nvim'})<cr>]], {})
