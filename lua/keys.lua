f0fe80d34dae7175d48ef1ac509c72fa4a8c4a26
Will Olson <will.olson11@gmail.com>
2022-09-23 11:00:33 -0700

    Add key map for opening a scratch note

--- lua/keys.lua
+++ lua/keys.lua
@@ -10,11 +10,22 @@ map('n', '<leader>n', [[:NvimTreeToggle]], {})
 map('n', '<leader>l', [[:IndentLinesToggle]], {})
 map('n', '<leader>t', [[:TagbarToggle]], {})
 
--- Misc
-map('i', '<leader><cr>', '<Esc>yyp<C-a>elC<Space>', {})     -- Auto increment markdown ordered list
-
 -- Fuzzy Find
 map('n', '<leader>ff', [[:Telescope find_files]], {})
 map('n', '<leader>hf', [[:lua require('telescope.builtin').find_files({cwd='~/'})<cr>]], {})
 map('n', '<leader>vf', [[:lua require('telescope.builtin').find_files({cwd='~/.config/nvim'})<cr>]], {})
 map('n', '<leader>nf', [[:lua require('telescope.builtin').find_files({cwd='~/Documents/notes'})<cr>]], {})
+
+-- Misc
+map('i', '<leader><cr>', '<Esc>yyp<C-a>elC<Space>', {})     -- Auto increment markdown ordered list
+vim.keymap.set('n', '<leader>sn', function()
+    local scratchNoteDirectory = "~/Documents/notes/scratch"
+    local date = os.date("%Y-%m-%d")
+    local filename = string.format(
+        "%s/%s.md",
+        scratchNoteDirectory,
+        date
+    )
+    
+    vim.cmd(string.format(":e %s", filename))
+end, {})
